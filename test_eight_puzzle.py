8c1b5638123f344421b8f8dd23d762d296f10114
shaharniz <shaharnizani.others@gmail.com>
2026-01-19 20:12:35 +0200

    Bug fix in testing is_goal

--- test_eight_puzzle.py
+++ test_eight_puzzle.py
@@ -90,7 +90,7 @@ class TestEightPuzzle(unittest.TestCase):
         the_goal = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]], dtype=np.uint8)
         assert EightPuzzle.is_goal(the_goal)
 
-    def test_is_goal_valid(self):
+    def test_is_goal_invalid(self):
         not_the_goal = np.array([[3, 4, 5], [0, 1, 2], [6, 7, 8]], dtype=np.uint8)
         assert not EightPuzzle.is_goal(not_the_goal)
 
